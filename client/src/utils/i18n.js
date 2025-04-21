3b7e3d7f9a6d6b38411676029803db5acde704ab
bekturmsv <itsbektur@gmail.com>
2025-04-22 00:26:52 +0200

    feat: add i18n for changing languages, 2 json with english & deutsch

---
+++ client/src/utils/i18n.js
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import resourcesToBackend from "i18next-resources-to-backend";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .use(
+    resourcesToBackend((language, namespace) =>
+      import(`../../public/locales/${language}/${namespace}.json`)
+    )
+  )
+  .init({
+    fallbackLng: "en",
+    ns: ["translation"],
+    defaultNS: "translation",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
