c8e48671731b559044b77d49fe62a1fb936bbc5a
Zhusup Akylbek <jojomen0039@gmail.com>
2025-04-30 01:30:56 +0200

    topic creation fix.

--- server/src/main/java/com/prog/datenbankspiel/dto/task/TopicDto.java
+++ server/src/main/java/com/prog/datenbankspiel/dto/task/TopicDto.java
@@ -6,4 +6,5 @@ import lombok.Data;
 @Data
 public class TopicDto {
     private String name;
+    private LevelDifficulty difficulty;
 }
--- server/src/main/java/com/prog/datenbankspiel/service/TopicServiceImpl.java
+++ server/src/main/java/com/prog/datenbankspiel/service/TopicServiceImpl.java
@@ -2,8 +2,9 @@ package com.prog.datenbankspiel.service;
 
 
 import com.prog.datenbankspiel.dto.task.TopicDto;
+import com.prog.datenbankspiel.model.task.Level;
 import com.prog.datenbankspiel.model.task.Topic;
-import com.prog.datenbankspiel.model.task.enums.LevelDifficulty;
+import com.prog.datenbankspiel.repository.task.LevelRepository;
 import com.prog.datenbankspiel.repository.task.TopicRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,16 @@ public class TopicServiceImpl implements TopicService {
 
     @Autowired
     private TopicRepository topicRepository;
+    @Autowired
+    private LevelRepository levelRepository;
 
     @Override
     public Topic createTopic(TopicDto topicDto) {
         Topic topic = new Topic();
         topic.setName(topicDto.getName());
+        topic.setDifficulty(topicDto.getDifficulty());
+        Level level = levelRepository.findByDifficulty(topicDto.getDifficulty());
+        topic.setLevel(level);
         return topicRepository.save(topic);
     }
 
